4856e40ab2901a51fe8dd395f23889371e3c487b
ihsan <ihsan24si@mahasiswa.pcr.ac.id>
2026-05-26 15:31:23 +0700

    feat: tambah komponen navbar

--- src/pages/FiturXyz.jsx
+++ src/pages/FiturXyz.jsx
@@ -1,21 +1,41 @@
-import { useState } from "react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import PageHeader from "../components/PageHeader";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 
 export default function FiturXyz() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
-    <div className="p-4">
-      <PageHeader title="Fitur-xyz" breadcrumb="Fitur-xyz">
-        <button 
-          onClick={() => setIsModalOpen(true)}
-          className="bg-hijau text-white px-4 py-2 rounded-lg font-bold hover:bg-green-600 transition-all"
-        >
-          + Add New Fitur
-        </button>
-      </PageHeader>
+    <div id="dashboard-container">
+      <PageHeader title="Fitur Xyz"/>
+      <p>Ini Halaman Fitur Xyz</p>
 
-     
+      <Button variant="outline">Batal</Button>
+      <Button variant="ghost">Batal</Button>
+      <Button variant="destructive">Batal</Button>
+
+            <Card className="mt-4 w-[380px]">
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Belajar shadcn/ui</CardTitle>
+            <Badge variant="secondary">Baru</Badge>
+          </div>
+          <CardDescription>
+            Contoh penggunaan komponen shadcn/ui di React
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <p className="text-sm text-muted-foreground">
+            Komponen ini dibuat di branch <strong>setup-shadcn</strong>
+            lalu di-merge ke main.
+          </p>
+        </CardContent>
+
+        <CardFooter className="flex gap-2">
+          <Button>Simpan</Button>
+          <Button variant="outline">Batal</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
